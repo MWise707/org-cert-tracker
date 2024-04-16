c86292b7aaeee153070deef63dcaa96872f5358a
MWise707 <markezrawiseman@gmail.com>
2024-04-15 17:28:27 -0700

    migration table

---
+++ server/migration.sql
@@ -0,0 +1,76 @@
+-- Create All Tables for certi-trax db 
+
+DROP TABLE IF EXISTS orgs CASCADE;
+DROP TABLE IF EXISTS branches CASCADE;
+DROP TABLE IF EXISTS sections CASCADE;
+DROP TABLE IF EXISTS teams CASCADE;
+DROP TABLE IF EXISTS individuals CASCADE;
+DROP TABLE IF EXISTS ind_cert_status CASCADE;
+DROP TABLE IF EXISTS ind_task_status CASCADE;
+DROP TABLE IF EXISTS certifications CASCADE;
+DROP TABLE IF EXISTS cert_tasks CASCADE;
+
+CREATE TABLE orgs (
+  org_id serial PRIMARY KEY,
+  org_name varchar
+);
+
+CREATE TABLE branches (
+  branch_id serial PRIMARY KEY,
+  branch_name varchar,
+  org_id int REFERENCES orgs (org_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE sections (
+  section_id serial PRIMARY KEY,
+  section_name varchar,
+  org_id int REFERENCES orgs (org_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  branch_id int REFERENCES branches (branch_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE teams (
+  team_id serial PRIMARY KEY,
+  team_name varchar,
+  org_id int REFERENCES orgs (org_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  branch_id int REFERENCES branches (branch_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  section_id int REFERENCES sections (section_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE individuals (
+  ind_id serial PRIMARY KEY,
+  ind_name varchar,
+  ind_position varchar,
+  ind_title varchar,
+  org_id int REFERENCES orgs (org_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  branch_id int REFERENCES branches (branch_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  section_id int REFERENCES sections (section_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  team_id int REFERENCES teams (team_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE certifications (
+  cert_id serial PRIMARY KEY,
+  cert_name varchar
+);
+
+CREATE TABLE cert_tasks (
+  cert_task_id serial PRIMARY KEY,
+  cert_name varchar
+);
+
+CREATE TABLE ind_cert_status (
+  ind_cert_status_id serial PRIMARY KEY,
+  ind_id int REFERENCES individuals (ind_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  cert_id int REFERENCES certifications (cert_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  is_cert_complete boolean,
+  percent_cert_complete numeric(3, 2)
+);
+
+CREATE TABLE ind_task_status (
+  ind_task_status_id serial PRIMARY KEY,
+  cert_task_id int REFERENCES cert_tasks (cert_task_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  ind_id int REFERENCES individuals (ind_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  is_task_passed boolean,
+  date_last_tested date,
+  ind_task_feedback text
+);
+
--- server/server.js
+++ server/server.js
@@ -2,3 +2,37 @@ import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
 
+dotenv.config({ path: "../.env"});
+
+const { PORT, DATABASE_URL } = process.env;
+
+const client = new pg.Client({
+  connectionString: DATABASE_URL,
+});
+
+(async () => {
+  await client.connect();
+  const app = express();
+  app.use(express.json());
+
+  app.get("/techs", async (req, res, next) => {
+    try {
+      const query = "SELECT * from techs";
+      const data = await client.query(query);
+      res.status(200).send(data.rows);
+    }
+    catch (error) {
+      console.error("Error with tech query", error);
+      next(error);
+    }
+  })
+
+  app.use((err, req, res, next) => {
+    console.error("Reached middleware error handling", err.stack);
+    res.status(500).send("Sorry - something went wrong!");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+})
